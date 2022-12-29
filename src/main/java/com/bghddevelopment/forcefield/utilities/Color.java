bed2f77b382ce800691bb97ac5156b9d9e0cf111
RyanMoodGAMING <ryan@ryanmood.com>
2022-12-29 18:08:47 +0000

    Add Color

---
+++ src/main/java/com/bghddevelopment/forcefield/utilities/Color.java
@@ -0,0 +1,35 @@
+package com.bghddevelopment.forcefield.utilities;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Color {
+
+    public static String translate(String message) {
+        if (Bukkit.getVersion().contains("1.16") || Bukkit.getVersion().contains("1.17") || Bukkit.getVersion().contains("1.18") || Bukkit.getVersion().contains("1.19") || Bukkit.getVersion().contains("1.20")) {
+            Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+            Matcher matcher = pattern.matcher(message);
+
+            while (matcher.find()) {
+                String color = message.substring(matcher.start(), matcher.end());
+                message = message.replace(color, net.md_5.bungee.api.ChatColor.valueOf(color) + "");
+                matcher = pattern.matcher(message);
+            }
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public static List<String> translate(List<String> source) {
+        return source.stream().map(Color::translate).collect(Collectors.toList());
+    }
+
+    public static void log(String message) {
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&7[&9BuildMode&7] " + message));
+    }
+
+}
